85582c1fe8c2e98a50b5edee6522e5b5bc77a6d7
toledofernando <toledof764@gmail.com>
2023-02-09 00:38:54 +0100

    hello

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -21,12 +22,21 @@ export default function Home() {
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
+            <button
+              onClick={() => {
+                axios
+                  .get("/api/data")
+                  .then((response) => console.log(response.data));
+              }}
+            >
+              Prueba
+            </button>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +129,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
